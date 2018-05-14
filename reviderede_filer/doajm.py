84f393f11e696b9d0f269b66b07e95158df89494
Asger Juel Hansen <enemydown-dk@users.noreply.github.com>
2018-05-14 12:28:33 +0200

    Add files via upload

--- reviderede_filer/doajm.py
+++ reviderede_filer/doajm.py
@@ -13,15 +13,14 @@ URL = 'https://doaj.org/api/v1/search/journals/' #URL to API'et
 INPUT_FILE = argv[1] #1st. argument containing name of input file.
 OUTPUT_FILE = argv[2] #2nd. argument containing name of output file.
 SEARCHF = argv[3] #Seatchterm with quotationmarks or without.
-ISSN = '' #Global variabel for current ISSN being processed.
 
-def pull_data_api(url):
+def pull_data_api(url, iden):
     """Execute request in API and print either found data or errorcode to terminal."""
-    req = urllib.request.Request(url)
+    req = urllib.request.Request(url + iden)
     try:
         response = urllib.request.urlopen(req)
         data = json.loads(response.read())
-        write_data(OUTPUT_FILE, data)
+        write_data(OUTPUT_FILE, data, iden)
         print(data)
         time.sleep(0.5)
 
@@ -30,20 +29,27 @@ def pull_data_api(url):
 
 def open_csv():
     """Open file with name INPUT_FILE and calls the function pull_data_api."""
-    global ISSN
     with open(INPUT_FILE, newline='') as _:
-        reader = csv.reader(_, delimiter=';')
+        reader = csv.reader(_, delimiter=',') #Note special delimitor.
         for row in reader:
-            templist = row[0].split(',')
-            ISSN = templist[0]
-            pull_data_api(URL + templist[0])
+            ISSN = row[0]
+            pull_data_api(URL, ISSN)
+            try:
+                EISSN = row[1].strip()
+                pull_data_api(URL, EISSN)
+            except IndexError:
+                #not enough elements in the line
+                continue
+    _.close()
 
-def write_data(json_name, data):
+def write_data(json_name, data, iden):
     """Write data to file with name OUTPUT_FILE (append mode)."""
     with open(json_name, mode="a") as file:
         try:
-            file.write(ISSN + ';' + data['results'][0]['bibjson'][SEARCHF]+'\n')
-        except IndexError as _:
+            file.write(iden + ';' + data['results'][0]['bibjson'][SEARCHF]+'\n')
+        except IndexError as _ie:
+            pass
+        except KeyError as _ke:
             pass
 
 def main():
